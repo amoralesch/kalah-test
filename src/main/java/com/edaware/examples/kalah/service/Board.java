f19269fdc8d15e9e722d9141f3e524d25bda951b
AMCH DEV <amch.dev@gmail.com>
2017-04-11 00:05:59 -0400

    Adding default constructor
    Adding basic constructor for the board.

--- src/main/java/com/edaware/examples/kalah/service/Board.java
+++ src/main/java/com/edaware/examples/kalah/service/Board.java
@@ -1,8 +1,19 @@
 package com.edaware.examples.kalah.service;
 
 public class Board {
+  private final int houses;
+  
   public Board(int houses) {
     if (houses <= 0)
       throw new IllegalArgumentException("invalid house number value");
+    
+    this.houses = houses;
+  }
+  
+  public int getTotalPits() {
+    // each player gets _houses_ pits (times 2)
+    // plus 1 store each (plus 2)
+    
+    return (houses * 2) + 2;
   }
 }
--- src/test/java/com/edaware/examples/kalah/service/BoardTest.java
+++ src/test/java/com/edaware/examples/kalah/service/BoardTest.java
@@ -1,5 +1,7 @@
 package com.edaware.examples.kalah.service;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
@@ -7,13 +9,19 @@ import org.junit.Test;
 public class BoardTest {
   @Test
   public void constructor_throwsException_whenSizeIsIncorrect()
-    throws Exception
-  {
+      throws Exception {
     try {
       new Board(0);
       fail("IllegalArgumentException expected");
     } catch (IllegalArgumentException ex) {
       // success
     }
+  }
+
+  @Test
+  public void constructor_createsValidBoard_always() throws Exception {
+    Board b = new Board(4);
+    
+    assertThat(b.getTotalPits(), equalTo(10));
   }
 }
