43bf7a71f7822b0ad5b8546ed2f777880580734c
AMCH DEV <amch.dev@gmail.com>
2017-04-22 22:25:02 -0400

    Adding logic to prevent giving seeds in the enemy store.

--- src/main/java/com/edaware/examples/kalah/service/Board.java
+++ src/main/java/com/edaware/examples/kalah/service/Board.java
@@ -5,37 +5,37 @@ public class Board {
     FIRST,
     SECOND
   }
-  
+
   private static final int DEFAULT_HOUSES = 6;
-  
+
   private static final int DEFAULT_SEEDS = 4;
 
   private final int houses;
-  
+
   private final int initialSeeds;
-  
+
   private int[] board;
-  
+
   private Player current = Player.FIRST;
-  
+
   public Board() {
     this(DEFAULT_HOUSES);
   }
-  
+
   public Board(int houses) {
     this(houses, DEFAULT_SEEDS);
   }
-  
+
   public Board(int houses, int seeds) {
     if (houses <= 0 || seeds <= 0)
       throw new IllegalArgumentException("invalid setup values");
-    
+
     this.houses = houses;
     initialSeeds = seeds;
-    
+
     initBoard();
   }
-  
+
   /**
    * Creates the board and set the correct amount of seeds in each pit
    */
@@ -43,13 +43,13 @@ public class Board {
     // each player gets _houses_ pits (times 2)
     // plus 1 store each (plus 2)
     board = new int[(houses * 2) + 2];
-    
+
     // the setup of the board will be:
     // [0] to [houses - 1] -> player one houses
     // [houses] -> player one store
     // [houses + 1] to [houses * 2] -> player two houses
     // [houses * 2 + 1] -> player 2 store
-    
+
     for (int i = 0; i < houses; i++) {
       board[i] = initialSeeds;
       board[i + houses + 1] = initialSeeds;
@@ -59,18 +59,18 @@ public class Board {
   public int getTotalPits() {
     return board.length;
   }
-  
+
   public int getInitialSeeds() {
     return initialSeeds;
   }
 
   /**
    * Return the number of seeds in one house
-   * 
+   *
    * @param player what side of the board to look at
    * @param house the relative position of the pit to look at;
    *    starting from 1 and up to the number of houses per player
-   *    
+   *
    * @return the number of seeds in the requested pit
    */
   public int getSeedCount(Player player, int house) {
@@ -80,29 +80,32 @@ public class Board {
   public Player getCurrentPlayer() {
     return current;
   }
-  
+
   public void makeMove(Player player, int house) {
     if (!isValidMove(player, house))
       return;
-   
+
     int seeds = takeAllSeeds(player, house);
 
     Player houseOwner = player;
     int nextHouse = house + 1;
     while (seeds > 0) {
       if (nextHouse > houses) {
-        addSeedsStore(houseOwner, 1);
-        
+        if (houseOwner == player)
+          addSeedsStore(houseOwner, 1);
+        else
+          seeds++;
+
         nextHouse = 1;
         houseOwner = getEnemy(houseOwner);
       } else {
         addSeedsPlayer(houseOwner, nextHouse, 1);
         nextHouse++;
       }
-      
+
       seeds--;
     }
-    
+
     // last seed was stored
     if (nextHouse != 1)
       switchPlayer();
@@ -114,21 +117,21 @@ public class Board {
     board[index] = 0;
     return seeds;
   }
-  
+
   private boolean isValidMove(Player player, int house) {
     if (player != current)
       return false;
-   
+
     return getSeedCount(player, house) > 0;
   }
-  
+
   private void addSeedsPlayer(Player player, int house, int seeds) {
     board[getIndexHouse(player, house)] += seeds;
   }
 
   /**
    * Adds some seeds to the player's store pit
-   * 
+   *
    * @param player the player owner of the store
    * @param seeds the number of seeds to add
    */
@@ -143,19 +146,19 @@ public class Board {
   private Player getEnemy(Player p) {
     return p == Player.FIRST ? Player.SECOND : Player.FIRST;
   }
-  
+
   /**
-   * Set the number of seeds in one house. 
+   * Set the number of seeds in one house.
    * Made protected for for Unit Tests
    */
   protected void setSeedCount(Player player, int house, int seeds) {
     board[getIndexHouse(player, house)] = seeds;
   }
-  
+
   public int getStoreCount(Player player) {
     return board[getIndexStore(player)];
   }
-  
+
   public boolean isGameOver() {
     return false;
   }
@@ -163,19 +166,19 @@ public class Board {
   private int getIndexHouse(Player player, int house) {
     if (house < 1 || house > houses)
       throw new IllegalArgumentException("house index is out of range");
-    
+
     return (house - 1) + getOffsetForPlayer(player);
   }
-  
+
   private int getIndexStore(Player player) {
     return player == Player.FIRST ?
         houses : (houses * 2) + 1;
   }
-  
+
   private int getOffsetForPlayer(Player player) {
     if (player == Player.FIRST)
       return 0;
-    
+
     // +1 = store for player one
     return houses + 1;
   }
--- src/test/java/com/edaware/examples/kalah/service/BoardTest.java
+++ src/test/java/com/edaware/examples/kalah/service/BoardTest.java
@@ -11,11 +11,11 @@ import com.edaware.examples.kalah.service.Board.Player;
 
 public class BoardTest {
   private static final int HOUSES = 3;
-  
+
   private static final int SEEDS = 2;
-  
+
   Board board = new Board(HOUSES, SEEDS);
- 
+
   @Test
   public void constructor_throwsException_whenSizeIsIncorrect()
       throws Exception {
@@ -35,23 +35,23 @@ public class BoardTest {
   }
 
   @Test
-  public void constructor_createsSixHouseBoardWithFourSeeds_byDefault() 
-      throws Exception 
+  public void constructor_createsSixHouseBoardWithFourSeeds_byDefault()
+      throws Exception
   {
     Board board = new Board();
-    
+
     assertThat(board.getTotalPits(), equalTo(14));
     assertThat(board.getInitialSeeds(), equalTo(4));
   }
-  
+
   @Test
   public void constructor_createsNeededPits_always() throws Exception {
     Board b = new Board(4);
-    
+
     assertThat(b.getTotalPits(), equalTo(10));
     assertThat(b.getInitialSeeds(), equalTo(4));
   }
-  
+
   @Test
   public void constructor_setInitialSeeds_always() throws Exception {
     Board b = new Board(4, 2);
@@ -59,19 +59,19 @@ public class BoardTest {
     assertThat(b.getTotalPits(), equalTo(10));
     assertThat(b.getInitialSeeds(), equalTo(2));
   }
-  
+
   @Test
   public void constructor_generateCorrectSetup_always() throws Exception {
     for (int i = 1; i <= HOUSES; i++)
       assertThat(board.getSeedCount(Player.FIRST, i), equalTo(SEEDS));
-    
+
     for (int i = 1; i <= HOUSES; i++)
       assertThat(board.getSeedCount(Player.SECOND, i), equalTo(SEEDS));
 
     assertThat(board.getStoreCount(Player.FIRST), equalTo(0));
     assertThat(board.getStoreCount(Player.SECOND), equalTo(0));
   }
-  
+
   @Test
   public void getSeedCount_throwsException_whenIndexIsWrong()
       throws Exception {
@@ -94,7 +94,7 @@ public class BoardTest {
   public void getNextPlayer_playerOne_whenStartingGame() throws Exception {
     assertThat(board.getCurrentPlayer(), equalTo(Player.FIRST));
   }
-  
+
   @Test
   public void makeMove_ignoresMove_whenIsNotPlayerTurn() throws Exception {
     board.makeMove(Player.SECOND, 1);
@@ -106,7 +106,7 @@ public class BoardTest {
   @Test
   public void makeMove_ignoresMove_whenSelectedHouseIsEmpty() throws Exception {
     board.setSeedCount(Player.FIRST, 1, 0);
-    
+
     board.makeMove(Player.FIRST, 1);
 
     assertThat(board.getCurrentPlayer(), equalTo(Player.FIRST));
@@ -118,7 +118,7 @@ public class BoardTest {
 
     assertThat(board.getSeedCount(Player.FIRST, 1), equalTo(0));
   }
-  
+
   @Test
   public void makeMove_addsSeedsNextHouses_always() throws Exception {
     board.makeMove(Player.FIRST, 1);
@@ -127,7 +127,24 @@ public class BoardTest {
     assertThat(board.getSeedCount(Player.FIRST, 2), equalTo(SEEDS + 1));
     assertThat(board.getSeedCount(Player.FIRST, 3), equalTo(SEEDS + 1));
   }
-  
+
+  @Test
+  public void makeMove_dontAddStone_inEnemyStore() throws Exception {
+    board.setSeedCount(Player.FIRST, HOUSES, HOUSES + 2);
+
+    board.makeMove(Player.FIRST, HOUSES);
+
+    assertThat(board.getSeedCount(Player.FIRST, HOUSES), equalTo(0));
+    assertThat(board.getStoreCount(Player.FIRST), equalTo(1));
+
+    assertThat(board.getSeedCount(Player.SECOND, 1), equalTo(SEEDS + 1));
+    assertThat(board.getSeedCount(Player.SECOND, 2), equalTo(SEEDS + 1));
+    assertThat(board.getSeedCount(Player.SECOND, 3), equalTo(SEEDS + 1));
+    assertThat(board.getStoreCount(Player.SECOND), equalTo(0));
+
+    assertThat(board.getSeedCount(Player.FIRST, 1), equalTo(SEEDS + 1));
+  }
+
   @Test
   public void makeMove_dontSwitchPlayers_whenLastStoneIsInStore()
       throws Exception {
@@ -136,19 +153,19 @@ public class BoardTest {
     assertThat(board.getSeedCount(Player.FIRST, 2), equalTo(0));
     assertThat(board.getSeedCount(Player.FIRST, 3), equalTo(SEEDS + 1));
     assertThat(board.getStoreCount(Player.FIRST), equalTo(1));
-    
+
     assertThat(board.getCurrentPlayer(), equalTo(Player.FIRST));
   }
-  
+
   @Test
   public void makeMove_switchesPlayers_whenValidMove() throws Exception {
     assertThat(board.getCurrentPlayer(), equalTo(Player.FIRST));
-    
+
     board.makeMove(Player.FIRST, 1);
 
     assertThat(board.getCurrentPlayer(), equalTo(Player.SECOND));
   }
-  
+
   @Test
   public void makeMove_addStonesToEnemy_whenTooMuchStones() throws Exception {
     board.makeMove(Player.FIRST, 3);
@@ -157,17 +174,17 @@ public class BoardTest {
     assertThat(board.getStoreCount(Player.FIRST), equalTo(1));
 
     assertThat(board.getSeedCount(Player.SECOND, 1), equalTo(SEEDS + 1));
-    
+
     assertThat(board.getCurrentPlayer(), equalTo(Player.SECOND));
   }
-  
+
   @Test
   public void isGameOver_false_whenStonesAreBothPlayersHouses()
       throws Exception {
     // some stones are still there
     assertThat(board.getSeedCount(Player.FIRST, 1), greaterThan(0));
     assertThat(board.getSeedCount(Player.SECOND, 1), greaterThan(0));
-    
+
     assertThat(board.isGameOver(), equalTo(false));
   }
 }
